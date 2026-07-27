1e96081e96a128d21e03532f3227fc3c05b65d0f
Kanchan <kanchanrathor05@gmail.com>
2026-07-27 22:49:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,109 @@
+// console.log("salempur");
+// console.info("mainpuri");
+// console.warn("bhopal");
+// console.error("kuraoli");
+
+// let a =prompt("name")
+// alert(`hey ${a}`)
+
+// let a = 22
+// typeof
+
+
+// let Username= prompt("what is your name?")
+// console.log(typeof Username);      // prompt se jo bhi loge bo string bn jayega
+
+// let age = prompt("age likho")  // jb ham age likhege to number mai likhege to bo usako string hi btayega
+
+// esko solve krne ka ek tarika hota hota h
+
+// let age = Number(prompt("Enter your age:"));
+
+// console.log(age);
+// console.log(typeof age);
+
+
+// js me array ka type Object hota h
+// exam= 
+// let skills=["js","html"]  eska typeof object
+
+// let x=null  typeof= object h eska bhi okk
+
+//NaN ka data type number
+
+
+
+
+// ============ PREMITIVE data type => jo value ko hold krte h 
+// exm String
+// Number
+// Boolean
+// Undefined
+// Null
+// BigInt
+// Symbol
+
+
+
+//   ============== Reference (Non-Primitive) Data Types  => (reference) ko store karte hain.
+// Object
+// Array
+// Function
+// Date
+// Map
+
+
+
+// ========= OPERATORS IN JS  ===========
+
+// ================= [1] ARITHMETIC OPERATOR ==============
+// + - * % ** / 
+
+// 2**3 => 2 ki power 3
+
+// [2]========== ASSIGNMENT OPERATOR =============
+//   = += -+ *+ /+ %=
+
+
+// [3] =========COMPARISON OPRATOR =========
+// == === >  <  >=  <= !=  !==
+
+
+// [4] ========= LOGICAL OPRETOR ==========
+// && || !
+
+
+// true && true=> true
+// false && true => false
+// true && false => false 
+// false && false => false 
+
+//  true || true => true
+// false || true => true
+// true || false => true
+// false || false => false
+
+
+
+//  ===============  TERNORY OPERATOR =============
+// conductio ? this dot : that dot
+
+
+//   WHAT IS HOISTING ?
+//  hoisting => variable ko banane se phle bhi use kr skte h 
+
+ // console.log(a);
+// var  a = 12   ans=> Undefiend
+// hoisting mai variable toot jata h do hisso mai jaise 
+// var a;  esako khte h declaration 
+// a=12; esako khte h initialization 
+
+// HOISTING let ke sath work nhi krti h *****
+
+//  console.log(b);  esko yaha nhi define kr skte h kyu yaha TDZ h 
+
+// let b =24;
+//  yaha kr skte ho define
+
+
+
